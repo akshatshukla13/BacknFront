de93307767c18f80ca1673851dd89a20297c83a1
Akshat-Sh <akshatvijay1302@gmail.com>
2024-07-17 00:44:19 +0530

    modified:   Frontend/src/components/Aside/Aside.jsx 	modified:   Frontend/src/components/VideoPage/EmptyVideoPage.jsx

--- Frontend/src/components/VideoPage/EmptyVideoPage.jsx
+++ Frontend/src/components/VideoPage/EmptyVideoPage.jsx
@@ -296,7 +296,7 @@ function EmptyVideoPage() {
           </div>
         </nav>
       </header>
-      <div class="flex min-h-[calc(100vh-66px)] sm:min-h-[calc(100vh-82px)]">
+      <div class=" flex min-h-[calc(100vh-66px)] sm:min-h-[calc(100vh-82px)]">
         <aside class="group fixed inset-x-0 bottom-0 z-40 w-full shrink-0 border-t border-white bg-[#121212] px-2 py-2 sm:absolute sm:inset-y-0 sm:max-w-[70px] sm:border-r sm:border-t-0 sm:py-6 sm:hover:max-w-[250px] lg:sticky lg:max-w-[250px]">
           <ul class="flex justify-around gap-y-2 sm:sticky sm:top-[106px] sm:min-h-[calc(100vh-130px)] sm:flex-col">
             <li class="">
