87dc32584c08bccd27e16760ce5d2b263227c9d6
Akshat-Sh <akshatvijay1302@gmail.com>
2024-07-20 00:11:39 +0530

    modified:   Frontend/vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,22 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
     proxy: {
       '/api': {
-        target: 'https://backnnfront.vercel.app/',
+        target: 'https://aktube.vercel.app',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-})
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
 
