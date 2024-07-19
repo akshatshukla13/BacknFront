2a5128fc2203ea284076397656132e6b875bbc62
Akshat-Sh <akshatvijay1302@gmail.com>
2024-07-19 22:19:07 +0530

    modified:   Backend/src/routes/video.routes.js

--- Backend/src/routes/video.routes.js
+++ Backend/src/routes/video.routes.js
@@ -1,12 +1,12 @@
-import { Router } from 'express';
+import { Router } from "express";
 import {
-    deleteVideo,
-    getAllVideos,
-    getVideoById,
-    publishAVideo,
-    togglePublishStatus,
-    updateVideo,
-} from "../controllers/video.controller.js"
+  deleteVideo,
+  getAllVideos,
+  getVideoById,
+  publishAVideo,
+  togglePublishStatus,
+  updateVideo,
+} from "../controllers/video.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -14,29 +14,29 @@ const router = Router();
 // router.use(); // Apply verifyJWT middleware to all routes in this file
 
 router
-    .route("/")
-    .get(getAllVideos)
-    .post(verifyJWT,
-        upload.fields([
-            {
-                name: "videoFile",
-                maxCount: 1,
-            },
-            {
-                name: "thumbnail",
-                maxCount: 1,
-            },
-            
-        ]),
-        publishAVideo
-    );
+  .route("/")
+  .get(getAllVideos)
+  .post(
+    verifyJWT,
+    upload.fields([
+      {
+        name: "videoFile",
+        maxCount: 1,
+      },
+      {
+        name: "thumbnail",
+        maxCount: 1,
+      },
+    ]),
+    publishAVideo
+  );
 
 router
-    .route("/:videoId")
-    .get(verifyJWT,getVideoById)
-    .delete(verifyJWT,deleteVideo)
-    .patch(verifyJWT,upload.single("thumbnail"), updateVideo);
+  .route("/:videoId")
+  .get(verifyJWT, getVideoById)
+  .delete(verifyJWT, deleteVideo)
+  .patch(verifyJWT, upload.single("thumbnail"), updateVideo);
 
-router.route("/toggle/publish/:videoId").patch(verifyJWT,togglePublishStatus);
+router.route("/toggle/publish/:videoId").patch(verifyJWT, togglePublishStatus);
 
-export default router
+export default router;
